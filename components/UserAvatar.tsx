d5afd6c5c507f71a7b375044ed6cb934a7599180
mohamedeld <mohamed.azoz20010@gmail.com>
2025-05-14 20:40:31 +0300

    handle logout redirect

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -10,8 +10,14 @@ import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "./ui/button";
 import { logout } from "@/actions/auth.action";
+import { useRouter } from "next/navigation";
 
 const UserAvatar = () => {
+    const router = useRouter();
+    const handleLogout = ()=>{
+        logout();
+        router.push("/login")
+    }
     return (
     <Popover>
       <PopoverTrigger className="cursor-pointer">
@@ -23,7 +29,7 @@ const UserAvatar = () => {
       <PopoverContent>
         <div className="flex items-center flex-col gap-4">
           <Link href="/profile" className="hover:bg-gray-500">Profile</Link>
-          <Button variant={"ghost"} className="hover:bg-gray-500" onClick={logout}>Logout</Button>
+          <Button variant={"ghost"} className="hover:bg-gray-500" onClick={handleLogout}>Logout</Button>
         </div>
       </PopoverContent>
     </Popover>
